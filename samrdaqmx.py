6ba827ebb050195620b44dfc98ccb7b23b4761e0
tatsunootoshigo <7475un00705hi90@gmail.com>
2019-09-27 18:31:43 +0200

    some improvements in the buffer size allocation for reader/writer tasks

--- samrdaqmx.py
+++ samrdaqmx.py
@@ -4,8 +4,8 @@
 #------------------------------------------------------#
 
 import nidaqmx
-from pynput import keyboard
-from pynput.keyboard import Key, KeyCode, Listener, Controller
+#from pynput import keyboard
+#from pynput.keyboard import Key, KeyCode, Listener, Controller
 from nidaqmx.constants import AcquisitionType, FuncGenType
 from matplotlib.widgets import CheckButtons, Button
 import numpy as np
@@ -29,24 +29,38 @@ for device in nidaqmx_sys.devices:
     print('Device Name: {0}, Product Category: {1}, Product Type: {2}'.format(device.name, device.product_category, device.product_type))
 
 # sine fucntion parameters = AC voltage
-fs = 50 #Hz
-T = 3
-nSamples = int(T * fs) #samples
+ac_nSamples = 10000 #samples
+ac_fs = 63 #Hz
+ac_a0 = 5
+ac_t0 = np.linspace(0, ac_nSamples, num=ac_nSamples)
+ac_sig = ac_a0*np.sin(2*np.pi*ac_fs*ac_t0)
 
-a0 = 5
-f0 = 3
-t0_ac = np.arange(nSamples)/fs
-sig = a0*np.sin(2*np.pi*f0*t0_ac)
+# dc pulse parameter = DC voltage
+#task_ch1_out.write([1.1, 1.1, 1.1, 1.1, 1.1, 1.1], auto_start=True)
+# terrible spike up to 3.5V if num=1001 or less for 1V
+#rise_1V = np.linspace(0.0, 1.0, num=2000)
+# nice fall without spikes even for num=1001 for 1V
+#stay_1V = np.ones(2000)
+#fall_1V = np.linspace(1.0, 0.0, num=2000)
+#dc_pulse = np.concatenate([rise_1V, stay_1V, fall_1V])
 
-# samples to read for ac_gen_volt signal
-ac_gen_volt_samples_per_chan_no = 250
-dc_gen_volt_samples_per_chan_no = 250
+dc_nSamples = 10000
+dc_fs = 34 #Hz
+dc_t0 = np.linspace(0, dc_nSamples, num=dc_nSamples)
+dc_sig = signal.square(2*np.pi*dc_fs*dc_t0, duty=0.5)
 
-# if these don't mach signal plots don't mach
-task_ch0_out_timing_rate = 100
-task_ch0_in_timing_rate = 100
-task_ch1_out_timing_rate = 20001
-task_ch1_in_timing_rate = 20001
+
+#dc_nSamples = np.size(dc_pulse) 
+
+# samples to read for ac_gen_volt ac_signal (size of buffer allocation)
+ac_gen_volt_samples_per_chan_no = 10*ac_nSamples
+dc_gen_volt_samples_per_chan_no = 10*dc_nSamples
+
+# if these don't mach ac_signal plots don't mach
+task_ch0_out_sampling_rate = 2*ac_nSamples
+task_ch0_in_sampling_rate = 1*ac_nSamples
+task_ch1_out_sampling_rate = 2*dc_nSamples
+task_ch1_in_sampling_rate = 1*dc_nSamples
 # tasks to be run by the aquisition card
 task_ch0_in = nidaqmx.Task() 
 task_ch1_in = nidaqmx.Task()
@@ -58,13 +72,16 @@ ch1_in_name = "Dev1/ai1"
 ch0_out_name = "Dev1/ao0"
 ch1_out_name = "Dev1/ao1"
 
+# multiple channels
+ch01_in_name = "Dev/ai0:1" 
+
 # input output channels on the card (consult piount)
 ch0_in = task_ch0_in.ai_channels.add_ai_voltage_chan(ch0_in_name)
 ch1_in = task_ch1_in.ai_channels.add_ai_voltage_chan(ch1_in_name)
 
-task_ch0_in.timing.cfg_samp_clk_timing(rate=task_ch0_in_timing_rate, sample_mode=AcquisitionType.CONTINUOUS)
+task_ch0_in.timing.cfg_samp_clk_timing(rate=task_ch0_in_sampling_rate, sample_mode=AcquisitionType.FINITE, samps_per_chan=ac_gen_volt_samples_per_chan_no)
 
-task_ch1_in.timing.cfg_samp_clk_timing(rate=task_ch1_in_timing_rate, sample_mode=AcquisitionType.CONTINUOUS)
+task_ch1_in.timing.cfg_samp_clk_timing(rate=task_ch1_in_sampling_rate, sample_mode=AcquisitionType.CONTINUOUS)
 
 # print(task_ch0_out.timing.samp_clk_rate)
 
@@ -86,13 +103,32 @@ def gen_ac_start(event):
 	print('Plotting selected AC datasets...')
 	task_ch0_out = nidaqmx.Task()
 	ch0_out = task_ch0_out.ao_channels.add_ao_voltage_chan(ch0_out_name)
-	task_ch0_out.timing.cfg_samp_clk_timing(rate=task_ch0_out_timing_rate, sample_mode=AcquisitionType.FINITE, samps_per_chan=ac_gen_volt_samples_per_chan_no)
+	task_ch0_out.timing.cfg_samp_clk_timing(rate=task_ch0_out_sampling_rate, sample_mode= AcquisitionType.FINITE, samps_per_chan=ac_gen_volt_samples_per_chan_no)
 
-	task_ch0_out.write(sig, auto_start=True)
+	task_ch0_out.write(ac_sig, auto_start=True)
 	task_ch0_out.wait_until_done()
 	task_ch0_out.stop()
 	task_ch0_out.close()
-	print(ac_df_fft)
+	#print(ac_df_fft)
+
+# defining button events for the plot
+def gen_dc_start(event):
+	#print(CheckButtons.get_status(chk_btn))
+	print('Plotting selected DC datasets...')
+	task_ch1_out = nidaqmx.Task()
+	task_ch1_out.ao_channels.add_ao_voltage_chan(ch1_out_name)
+	task_ch1_out.timing.cfg_samp_clk_timing(rate=task_ch1_out_sampling_rate, sample_mode= AcquisitionType.FINITE, samps_per_chan=dc_gen_volt_samples_per_chan_no)
+	#task_ch1_out.timing.cfg_samp_clk_timing(task_ch1_out_sampling_rate)
+
+	print(dc_sig)
+	task_ch1_out.write(dc_sig, auto_start=True)
+	#time.sleep(5)
+	#print(task_ch1_out.write(fall_1V, auto_start=True))
+	#task.stop()
+	task_ch1_out.wait_until_done()
+	task_ch1_out.stop()
+	task_ch1_out.close()
+	#print(ac_df_fft)	
 
 def gen_ac_clear(event):
 	#print(CheckButtons.get_status(chk_btn))
@@ -114,13 +150,17 @@ ax = plt.gca()
 #	label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65 ))
 
 # plot buttons
+plot_dc_btn_ax = plt.axes([0.01, 0.34, 0.06, 0.03], frameon=True)
+plot_dc_btn = Button(plot_dc_btn_ax, 'DC start', color='0.85', hovercolor='0.95')
+plot_dc_btn.on_clicked(gen_dc_start)
+
 plot_ac_btn_ax = plt.axes([0.01, 0.30, 0.06, 0.03], frameon=True)
 plot_ac_btn = Button(plot_ac_btn_ax, 'AC start', color='0.85', hovercolor='0.95')
 plot_ac_btn.on_clicked(gen_ac_start)
 
-plot_dc_btn_ax = plt.axes([0.01, 0.26, 0.06, 0.03], frameon=True)
-plot_dc_btn = Button(plot_dc_btn_ax, 'AC clear', color='0.85', hovercolor='0.95')
-plot_dc_btn.on_clicked(gen_ac_clear)
+plot_ac_clear_btn_ax = plt.axes([0.01, 0.26, 0.06, 0.03], frameon=True)
+plot_ac_clear_btn = Button(plot_ac_clear_btn_ax, 'AC clear', color='0.85', hovercolor='0.95')
+plot_ac_clear_btn.on_clicked(gen_ac_clear)
 
 exit_btn_ax = plt.axes([0.01, 0.22, 0.06, 0.03], frameon=True)
 exit_btn = Button(exit_btn_ax, 'Exit', color='0.85', hovercolor='0.95')
@@ -130,9 +170,9 @@ plt.ion()
 
 aq_start = True
 while aq_start == True:
-	acvolt_in = task_ch0_in.read(number_of_samples_per_channel=np.size(t0_ac))
+	acvolt_in = task_ch0_in.read(number_of_samples_per_channel=ac_nSamples)
 	time.sleep(1)
-	actime_in = t0_ac
+	actime_in = ac_t0
 	ac_data_row = pd.Series(acvolt_in, actime_in)
 	
 	data_row = pd.Series(acvolt_in, actime_in)
@@ -140,7 +180,7 @@ while aq_start == True:
 
 	ac_gen_xy.clear()
 	# plot the defined sinusoidal ac voltage function
-	ac_gen_xy.plot(t0_ac, sig, color="blue", linewidth=0.5, linestyle="-")
+	ac_gen_xy.plot(ac_t0, ac_sig, color="blue", linewidth=0.5, linestyle="-")
 	ac_gen_xy.plot(ac_df, color='r', linewidth=1.0)
 
 	ac_df_fft = np.fft.fft(ac_df)
